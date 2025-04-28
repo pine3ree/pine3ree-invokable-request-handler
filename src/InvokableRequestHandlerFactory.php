8972ce9b2cd944604ddc6ad41e22d36b4b6661c3
pine3ree <pine3ree@gmail.com>
2025-04-29 01:20:14 +0200

    Fetch or create a ParamsResolverInterface instance
    Signed-off-by: pine3ree <pine3ree@gmail.com>

--- src/InvokableRequestHandlerFactory.php
+++ src/InvokableRequestHandlerFactory.php
@@ -9,16 +9,30 @@ namespace pine3ree\Http\Server;
 
 use Psr\Container\ContainerInterface;
 use pine3ree\Container\ParamsResolver;
+use pine3ree\Container\ParamsResolverInterface;
 use pine3ree\Http\Server\InvokableRequestHandler;
 
 /**
  * A generic factory for invokable-handlers which use method-injection for
  * dependencies using reflection
+ *
+ * The invokable-handler class constructor must only accept a single argument of
+ * type ContainerInterface
  */
 class InvokableRequestHandlerFactory
 {
     public function __invoke(ContainerInterface $container, string $handlerFQCN): InvokableRequestHandler
     {
-        return new $handlerFQCN($container->get(ParamsResolver::class));
+        // Fetch the params-resolver service from container
+        $paramsResolver = $container->has(ParamsResolverInterface::class)
+            ? $container->get(ParamsResolverInterface::class)
+            : null;
+
+        // If not found create a new one
+        if ($paramsResolver === null || ! $paramsResolver instanceof ParamsResolverInterface) {
+            $paramsResolver = new ParamsResolver($container);
+        }
+
+        return new $handlerFQCN($paramsResolver);
     }
 }
