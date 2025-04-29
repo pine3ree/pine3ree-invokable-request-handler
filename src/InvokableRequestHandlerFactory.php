c3540f8122a531d752893a213da3b6ae51ff5e69
pine3ree <pine3ree@gmail.com>
2025-04-29 18:24:09 +0200

    Add params-resolver cache by container instance
    Signed-off-by: pine3ree <pine3ree@gmail.com>

--- src/InvokableRequestHandlerFactory.php
+++ src/InvokableRequestHandlerFactory.php
@@ -8,6 +8,7 @@
 namespace pine3ree\Http\Server;
 
 use Psr\Container\ContainerInterface;
+use SplObjectStorage;
 use pine3ree\Container\ParamsResolver;
 use pine3ree\Container\ParamsResolverInterface;
 use pine3ree\Http\Server\InvokableRequestHandler;
@@ -21,16 +22,25 @@ use pine3ree\Http\Server\InvokableRequestHandler;
  */
 class InvokableRequestHandlerFactory
 {
+    private ?SplObjectStorage $cache = null;
+
     public function __invoke(ContainerInterface $container, string $handlerFQCN): InvokableRequestHandler
     {
-        // Fetch the params-resolver service from container
-        $paramsResolver = $container->has(ParamsResolverInterface::class)
-            ? $container->get(ParamsResolverInterface::class)
-            : null;
+        $cache = $this->cache ?? $this->cache = new SplObjectStorage();
 
-        // If not found create a new one
-        if ($paramsResolver === null || ! $paramsResolver instanceof ParamsResolverInterface) {
-            $paramsResolver = new ParamsResolver($container);
+        /** @var SplObjectStorage $cache Previous line ensures this */
+        if ($cache->contains($container)) {
+            $paramsResolver = $cache->offsetGet($container);
+        } else {
+            if ($container->has(ParamsResolverInterface::class)) {
+                $paramsResolver = $container->get(ParamsResolverInterface::class);
+                if ($paramsResolver instanceof ParamsResolverInterface !== true) {
+                    $paramsResolver = new ParamsResolver($container);
+                }
+            } else {
+                $paramsResolver = new ParamsResolver($container);
+            }
+            $cache->attach($container, $paramsResolver);
         }
 
         return new $handlerFQCN($paramsResolver);
