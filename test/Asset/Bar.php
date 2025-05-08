c1b62baec9275ff2a2cb45cf767dd6a70c43c5b7
pine3ree <pine3ree@gmail.com>
2025-05-08 23:50:40 +0200

    Add strict type declaration
    Signed-off-by: pine3ree <pine3ree@gmail.com>

--- test/Asset/Bar.php
+++ test/Asset/Bar.php
@@ -6,6 +6,7 @@
  * @author      pine3ree https://github.com/pine3ree
  */
 
+declare(strict_types=1);
 
 namespace pine3ree\test\Http\Server\Asset;
 
--- test/Asset/InvalidHandler.php
+++ test/Asset/InvalidHandler.php
@@ -6,6 +6,8 @@
  * @author      pine3ree https://github.com/pine3ree
  */
 
+declare(strict_types=1);
+
 namespace pine3ree\test\Http\Server\Asset;
 
 use Psr\Http\Message\ServerRequestInterface;
